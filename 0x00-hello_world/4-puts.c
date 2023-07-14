a5c03416e0a98860796b6abbb3be57db15e411ce
AyoubMatich <matichayyoub88@gmail.com>
2023-07-13 19:25:43 -0700

    up4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /**
- * this a smole programe print Programming is like building a multilingual puzzle
- */ 
+ *main -Entry point
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
