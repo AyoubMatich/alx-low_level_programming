7b722c2e0879c0ab01652439ebb195e859eb518f
AyoubMatich <matichayyoub88@gmail.com>
2023-07-13 19:13:33 -0700

    up4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+  this a smole programe print Programming is like building a multilingual puzzle
+ */ 
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
+ return 0;
+}
