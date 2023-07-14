886159526d5da18a01a860846957d083d53917b2
AyoubMatich <matichayyoub88@gmail.com>
2023-07-13 19:22:27 -0700

    up4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,8 @@
 /**
  * this a smole programe print Programming is like building a multilingual puzzle
  */ 
-int main(void) {
-puts("\"Programming is like building a multilingual puzzle");
- return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
