a66f31c0d3c21dd956111e5848e538be48c66416
AyoubMatich <matichayyoub88@gmail.com>
2023-09-30 22:50:34 +0100

    write function 1-

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,23 +1,17 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
+ * _print_rev_recursion - Prints a string in reverse
  * @s: string
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	if *s == '\n' 
+	if (*s != '\0')
 	{
-		_putchar('\n');
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	else if (*s == 0)
-	{
-		
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
