7146457ca6c0d3749b004a24468cc80f5e47f307
AyoubMatich <matichayyoub88@gmail.com>
2023-10-20 02:34:41 +0100

    code_1 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,2 +1,19 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+#include "stddef.h"  // Include for size_t, which is used in the function declarations
+
+/* Define the structure for a singly linked list node */
+typedef struct list_s {
+    char *str;
+    struct list_s *next;
+} list_t;
 
 list_t *add_node(list_t **head, const char *str);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+void first(void);
+
+#endif
