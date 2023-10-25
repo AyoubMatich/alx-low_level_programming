91010484524b2d87ab7b62b7aae650b254f532f8
samsung <ayyou@localhost.localdomain>
2023-10-25 01:20:50 +0100

    update 2 files 13 c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -19,24 +19,3 @@ size_t print_listint(const listint_t *h)
 
  return (num);
 }
-#include "lists.h"
-
-/**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
- size_t num = 0;
-
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
-}
